a34faf342865a08605061ee15c475da11e2c983d
Luis Ferrer-Labarca <ferrerluis97@gmail.com>
2017-02-04 02:58:44 -0500

    Configured sample home page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,22 +1,7 @@
 class MembersController < ApplicationController
-  before_action :set_member, only: [:show, :edit, :update, :destroy]
-
-  # GET /members
-  def index
-    @members = Member.all
-  end
-
-  # GET /members/1
-  def show
-  end
-
   # GET /members/new
   def new
     @member = Member.new
-  end
-
-  # GET /members/1/edit
-  def edit
   end
 
   # POST /members
@@ -24,33 +9,13 @@ class MembersController < ApplicationController
     @member = Member.new(member_params)
 
     if @member.save
-      redirect_to @member, notice: 'Member was successfully created.'
+      redirect_to home_url, notice: 'Member was successfully created.'
     else
       render :new
     end
   end
 
-  # PATCH/PUT /members/1
-  def update
-    if @member.update(member_params)
-      redirect_to @member, notice: 'Member was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /members/1
-  def destroy
-    @member.destroy
-    redirect_to members_url, notice: 'Member was successfully destroyed.'
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_member
-      @member = Member.find(params[:id])
-    end
-
     # Only allow a trusted parameter "white list" through.
     def member_params
       params.require(:member).permit(:first_name, :last_name, :email)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  controller :pages do
+    get '/' => :home, as: :home
+  end
 end
