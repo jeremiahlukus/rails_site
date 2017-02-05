4ffca563116b6544de3e3966d209550034debc41
Luis Ferrer-Labarca <ferrerluis97@gmail.com>
2017-02-04 23:38:31 -0500

    Added basic helpers to homepage

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+  def is_home
+    current_page?(home_url)
+  end
+
+  def nav_menu_items
+    [
+      {title: 'About', url: home_url},
+      {title: 'Events', url: home_url},
+      {title: 'Calendar', url: home_url},
+      {title: 'Blog', url: home_url}
+    ]
+  end
 end
