4ffc5752f8b10abf7db2f7de1903343b369f9544
luis <ferrerluis97@gmail.com>
2017-02-19 12:28:05 -0500

    Translated index blog page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,26 @@
 class Post
   REPO = 'pantherhackers/blog'.freeze
   CONFIG_LIMIT = '---'.freeze
+  RAW_GH = 'https://raw.githubusercontent.com/PantherHackers/blog/master/'.freeze
+  IGNORED = ['README.md', '.gitignore']
+
+  attr_reader :name, :author, :title, :thumbnail, :date, :tags, :published, :content, :url
 
   def initialize(f)
     raw_config, raw_content = Unirest.get(f.download_url).body.split(CONFIG_LIMIT)
     config = JSON.parse(raw_config, :symbolize_names => true)
-    @author = config[:author]         # e.g. "Aimee Vu"
-    @title = config[:title]           # e.g. "HackATL: Busy Making Businesses!"
-    @date = DateTime.iso8601(config[:date])       # ISO 8601 format e.g. "2017-02-15T04:24:38+00:00"
-    @tags = config[:tags]             # e.g. "learning", "hackathons", "events"
-    @published = config[:published]   # e.g. true, false
+    @name = f.name                                                  # e.g "hackatl-busy-making-businesses"
+    @author = config[:author]                                       # e.g. "Aimee Vu"
+    @title = config[:title]                                         # e.g. "HackATL: Busy Making Businesses!"
+    @thumbnail = "#{RAW_GH}images/#{config[:thumbnail]}".freeze if config[:thumbnail] # e.g. "pic.jpg"
+    @date = DateTime.iso8601(config[:date])                         # ISO 8601 format e.g. "2017-02-15T04:24:38+00:00"
+    @tags = config[:tags]                                           # e.g. "learning", "hackathons", "events"
+    @published = config[:published]                                 # e.g. true, false
     @content = raw_content
+    @url = "blog/#{@name}".freeze
   end
 
   def self.all
-    Octokit.contents(REPO).reject { |f| f.name == 'README.md' }.map { |f| Post.new(f) }
+    Octokit.contents(REPO).reject { |f| IGNORED.include?(f.name) || f.type != 'file' }.map { |f| Post.new(f) }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  controller :posts do
+    get '/blog' => :index, as: :index
+  end
+
   controller :pages do
     get '/' => :home, as: :home
   end
