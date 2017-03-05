867ed0227c64d3099c816ccc5d134bab6945d4b3
Brian Cabigon <briancabigon@gmail.com>
2017-03-05 05:52:33 -0500

    Fix Event model logic

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ require 'googleauth/stores/file_token_store'
 require 'fileutils'
 
 OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
-APPLICATION_NAME = 'Google Calendar API Ruby Quickstart'
+APPLICATION_NAME = 'PantherHackers rails_site'
 CLIENT_SECRETS_PATH = 'client_secret.json'
 CREDENTIALS_PATH = File.join(Dir.home, '.credentials', "calendar-ruby-quickstart.yaml")
 SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
@@ -16,11 +16,12 @@ class Event
   attr_reader :name, :start_datetime, :end_datetime, :description, :location, :link
 
 	def initialize(event_json)
-		@name = event_json.name
+	  @name = event_json.name
 		@start_datetime = event_json.start
 		@end_datetime = event_json.end
 		@location = event_json.location
 		@description = event_json.description
+		@link = event_json.description.split.first
 	end
 
 	def authorize
@@ -54,17 +55,19 @@ class Event
 		# Fetch the next 10 events for the user
 		calendar_id = 'primary'
 		response = service.list_events(calendar_id,
-		                               max_results: 10,
-		                               single_events: true,
+		                               single_events: false,
 		                               order_by: 'startTime',
-		                               time_min: Time.now.iso8601)
+		                               time_min: Time.now - 1.month, 
+		                               time_max: Time.now + 1.month)
 
 		puts "Upcoming events:"
 		puts "No upcoming events found" if response.items.empty?
+		event_array = Array.new
 		response.items.each do |event|
 		  start = event.start.date || event.start.date_time
-		  puts "- #{event.summary} (#{start})"
+		  event_array.push(Event.new(event))
 		end
+		event_array
 	end
 end
 	
