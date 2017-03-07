8bdb336bb8c6de8ce42e123aa18916d3e98f6f0f
luis <ferrerluis97@gmail.com>
2017-03-07 01:56:36 -0500

    Added Slack and Github buttons to social icons on navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,15 @@ module ApplicationHelper
       {title: 'Blog', url: blog_url}
     ]
   end
+
+  def social_menu_items
+    [
+      { icon: 'fa-facebook', url: 'https://www.facebook.com/PantherHackers' },
+      { icon: 'fa-twitter', url: 'https://twitter.com/PantherHackers' },
+      { icon: 'fa-instagram', url: 'https://instagram.com/pantherhackers/' },
+      { icon: 'fa-github', url: 'https://github.com/pantherhackers' },
+      { icon: 'fa-slack', url: 'https://pantherhackers.slack.com' },
+      { icon: 'fa-envelope-o', url: 'mailto:hello@pantherhackers.com' }
+    ]
+  end
 end
