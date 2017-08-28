aedeb852d4b43429b83f585f9411019342c620e4
Brian Cabigon-Soliber <brian.cabigonsoliber@ge.com>
2017-08-28 19:27:32 -0400

    Refactored event handling to consider edge cases

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
+require 'httparty'
 class Event
 
   attr_reader :name, :start, :end, :description, :location, :link, :thumbnail
@@ -22,28 +23,40 @@ class Event
     }
 
     response = HTTParty.get('https://www.googleapis.com/calendar/v3/calendars/calendarId/events', params)
-
+    puts response
     response['items'].reject{|item| item['status'] == "cancelled"}.map{ |event| 
-                   begin
-                    #Continue if date is in dateTime format
-                     Event.new(event['summary'], 
-                     DateTime.iso8601(event.dig('start','dateTime')), 
-                     DateTime.iso8601(event.dig('end','dateTime')), 
-                     event['location'], 
-                     event['description'].to_s.split[2..-1].join(' '), 
-                     event['description'].to_s.split.first, 
-                     event['description'].to_s.split.second)
-                   rescue
-                     #If not, then get dateTime
-                     #from date yyyy-mm-dd format
-                     Event.new(event['summary'], 
-                     DateTime.iso8601(event.dig('start', 'date')), 
-                     DateTime.iso8601(event.dig('end', 'date')), 
-                     event['location'], 
-                     event['description'].to_s.split[2..-1].join(' '), 
-                     event['description'].to_s.split.first, 
-                     event['description'].to_s.split.second)
-                   end
+                  event_summary = event['summary']
+                  event_start_dt = nil
+                  event_end_dt = nil
+                  event_loc = event['location']
+                  event_desc = nil
+                  event_sign_up_link = nil
+                  event_thumbnail = nil
+
+                  # Rescue for GCalendar date formatting
+                  begin
+                    event_start_dt = DateTime.iso8601(event.dig('start', 'dateTime'))
+                    event_end_dt = DateTime.iso8601(event.dig('end', 'dateTime'))
+                  rescue
+                    event_start_dt = DateTime.iso8601(event.dig('start', 'date'))
+                    event_end_dt = DateTime.iso8601(event.dig('end', 'date'))
+                  end
+
+                  # Rescue for GCalendar description 
+
+                  # PHEveCom was asked to put the description in this format for ease of parsing
+                  # event_sign_up_link event_thumbnail event_desc
+                  # where event_desc = every word after the first two links
+                  begin
+                    event_desc = event['description'].to_s.split[2..-1].join(' ')
+                    event_sign_up_link = event['description'].to_s.split.first
+                    event_thumbnail = event['description'].to_s.split.second
+                  rescue
+                    event_desc = event['description'].to_s
+                  end
+
+                   Event.new(event_summary, event_start_dt, event_end_dt, event_loc, 
+                      event_desc, event_sign_up_link, event_thumbnail)
     }
   end
 end
